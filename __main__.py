7f60e41be8e212c00b95fe6a3ae03af426ae8990
raven <you@example.com>
2015-09-27 01:25:07 +0200

    more testing and fixing, threading not working yet.

--- __main__.py
+++ __main__.py
@@ -1,50 +1,56 @@
 import RPi.GPIO as gpio
-import subprocess
+import threading
 from time import sleep
 
-gpio_in_doorbell = 11
-gpio_in_lampoverride = 12
-gpio_in_hornoverride = 13
-gpio_in_printoverride = 14
-gpio_in_confirm = 17
+gpio.setmode(gpio.BOARD)
 
-gpio_out_warninglamp = 15
-gpio_out_horn = 20
-gpio_out_led_doorbell = 18
-gpio_out_led_confirm = 19
+gpio_in_doorbell = 37
+gpio_in_lampoverride = 35
+gpio_in_hornoverride = 33
+gpio_in_printoverride = 31
+gpio_in_confirm = 29
+
+gpio_out_warninglamp = 40
+gpio_out_horn = 38
+gpio_out_led_doorbell = 36
+gpio_out_led_confirm = 32
 
 def setup_gpios():
-    gpio.setup(gpio_in_doorbell, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    gpio.setup(gpio_in_lampoverride, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    gpio.setup(gpio_in_hornoverride, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    gpio.setup(gpio_in_printoverride, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-    gpio.setup(gpio_in_confirm, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    gpio.setup(gpio_in_doorbell, gpio.IN, pull_up_down=gpio.PUD_UP)
+    gpio.setup(gpio_in_lampoverride, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+    gpio.setup(gpio_in_hornoverride, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+    gpio.setup(gpio_in_printoverride, gpio.IN, pull_up_down=gpio.PUD_DOWN)
+    gpio.setup(gpio_in_confirm, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 
-    gpio.setup(gpio_out_warninglamp, GPIO.OUT)
-    gpio.setup(gpio_out_horn, GPIO.OUT)
-    gpio.setup(gpio_out_led_doorbell, GPIO.OUT)
-    gpio.setup(gpio_out_led_confirm, GPIO.OUT)
+    gpio.setup(gpio_out_warninglamp, gpio.OUT)
+    gpio.setup(gpio_out_horn, gpio.OUT)
+    gpio.setup(gpio_out_led_doorbell, gpio.OUT)
+    gpio.setup(gpio_out_led_confirm, gpio.OUT)
 
 def wait_for_confirmation():
+    print('waiting for confirmation')
     def blink_confirm():
         gpio.add_event_detect(gpio_in_confirm, gpio.RISING, bouncetime=200)
         while not gpio.event_detected(gpio_in_confirm):
-            gpio.output(gpio_out_led_confirm, TRUE)
+            gpio.output(gpio_out_led_confirm, True)
             sleep(500)
-            gpio.output(gpio_out_led_confirm, FALSE)
+            gpio.output(gpio_out_led_confirm, False)
             sleep(500)
         gpio.remove_event_detect(gpio_in_confirm)
-    subprocess.Popen(blink_confirm())
+        threading.Thread(target=lambda: blink_confirm())
     gpio.wait_for_edge(gpio_in_confirm, gpio.RISING)
+    print('got confirmation')
 
 
 def run_warninglamp(time, confirm):
+    print('run_warninglamp %s.'%time)
     if gpio.input(gpio_in_lampoverride):
-        gpio.output(gpio_out_warninglamp, TRUE)
+        gpio.output(gpio_out_warninglamp, True)
         sleep(time)
         if confirm:
             wait_for_confirmation()
-        gpio.output(gpio_out_warninglamp, FALSE)
+        gpio.output(gpio_out_warninglamp, False)
+    print('ran warninglamp')
 
 def sound_horn(pattern, time):
     #TODO
@@ -52,10 +58,10 @@ def sound_horn(pattern, time):
 
 def eval_doorbell(channel):
     print('Edge detected on channel %s, eval_doorbell.'%channel)
-    gpio.output(gpio_out_led_doorbell, TRUE)
-    subprocess.Popen(run_warninglamp(10, 0))
-    gpio.wait_for_edge(gpio_in_doorbell, gpio.RISING)
-    gpio.output(gpio_out_led_doorbell, FALSE)
+#    gpio.output(gpio_out_led_doorbell, True)
+    threading.Thread(target=lambda: run_warninglamp(10, 0))
+#    gpio.wait_for_edge(gpio_in_doorbell, gpio.RISING)
+#    gpio.output(gpio_out_led_doorbell, False)
 
 
 def eval_phone():
@@ -64,5 +70,10 @@ def eval_phone():
     #run_warninglamp(1)
     pass
 
-
+setup_gpios()
 gpio.add_event_detect(gpio_in_doorbell, gpio.FALLING, callback=eval_doorbell, bouncetime=200)
+eval_doorbell(-1)#FIXME
+while True:
+    gpio.output(gpio_out_led_confirm, not gpio.input(gpio_in_doorbell))
+    sleep(.1)
+
